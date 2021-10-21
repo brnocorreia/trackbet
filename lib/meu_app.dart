2512124729ca5948d0910dd30aa4cfe559f75220
Bruno Correia <dev.brunocorreia@gmail.com>
2021-10-21 11:26:00 -0300

    fonts and images

---
+++ lib/meu_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:trackbet/pages/onboarding_page.dart';
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const OnboardingPage(),
+    );
+  }
+}
